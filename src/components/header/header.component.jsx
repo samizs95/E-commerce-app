1cc25ebca4ccba22795aef4fc8077e3afda7dba1
samizs95 <sarmai.zsolt@gmail.com>
2020-06-09 16:51:15 +0200

    Added cartIcon and cartdropdown option for my app using Redux for state management

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,11 +6,17 @@ import { Link } from "react-router-dom";
 
 import {ReactComponent as Logo} from "../../assets/4.4 crown.svg.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+
 import "./header.styles.scss";
 
 import { auth } from '../../Firebase/firebase.utils';
 
-const Header = ({currentUser})=> (
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+
+
+const Header = ({currentUser,hidden})=> (
     <div className='header'>
         <Link className="logo-container" to ="/">
             <Logo className="logo" />
@@ -28,11 +34,17 @@ const Header = ({currentUser})=> (
             :
             <Link className='option' to='/signin'>SIGN IN</Link>
         }
+        <CartIcon />
         </div>
+        {
+            hidden ? null :
+        <CartDropdown />
+        }
     </div>
 )
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);

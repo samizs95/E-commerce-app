5be0fe12b3ed109c8a08b2f37be5e68280118544
samizs95 <sarmai.zsolt@gmail.com>
2020-06-09 15:38:00 +0200

    Applied render to home if signed in successfully

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
-import {Switch, Route } from "react-router-dom";
+import {Switch, Route,Redirect } from "react-router-dom";
 import {connect} from 'react-redux';
 
 import ShopPage from "./pages/shop/shop.component";
@@ -47,15 +47,19 @@ class App extends React.Component {
       <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInandSignUpPage} />
+      <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInandSignUpPage />)} />
       </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
